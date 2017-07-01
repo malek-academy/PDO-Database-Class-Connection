1a30a968f8c574dd0660961570937d525ed98ccd
Malek Academy <malek.alawamrh@gmail.com>
2017-07-01 22:00:46 +0200

    Create Config.php

---
+++ Config.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Configuration File
+ *
+ * PHP Version 5.6
+ *
+ * @category  N/A
+ * @package   N/A
+ * @author    Malek alawamrh <malek.alawamrh@gmail.com>
+ * @copyright 2017 Malek Academy.co
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version   1.0
+ * @website   http://malek-academy.com
+ */
+
+// Set Database Host
+const DB_HOST = "localhost";
+
+// Set Database Username
+const DB_USER = "root";
+
+// Set Database Password
+const DB_PASS = "";
+
+// Set Database Name 
+const DB_NAME = "database";
+
+// Set Database Charset :: Preferred (UTF8)
+const CHARSET = "UTF8";
+
+// Set Server Default Time Zone
+
+date_default_timezone_set("Asia/Amman");
+
+?>
